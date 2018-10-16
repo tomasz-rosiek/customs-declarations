f55e9bccc46e177363b6f18d38d3927144e929a6
Marcin Cichocki <marcin.cichocki@digital.hmrc.gov.uk>
2018-10-16 11:14:03 +0100

    comment

--- app/uk/gov/hmrc/customs/declaration/services/filetransmission/queue/MongoBackedWorkItemService.scala
+++ app/uk/gov/hmrc/customs/declaration/services/filetransmission/queue/MongoBackedWorkItemService.scala
@@ -92,6 +92,8 @@ class MongoBackedWorkItemService @Inject()(
 
   private def nextAvailabilityTime[T](workItem: WorkItem[T]): DateTime = {
 
+    //TODO MC use akka predefined strategies
+
     val multiplier = Math.pow(2, workItem.failureCount).toInt
 
 //    val delay = Duration.standardSeconds(1) * multiplier //TODO MC hardcoded
